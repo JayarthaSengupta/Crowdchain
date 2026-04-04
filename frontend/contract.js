c6c85c8e016ae6dbc30193bf5d689d283af9b747
Jayartha Sengupta <jayarthasen31@gmail.com>
2026-04-04 19:00:17 +0530

    Added to contracts in Frontend

--- frontend/contract.js
+++ frontend/contract.js
@@ -2,7 +2,7 @@
 // ─────────────────────────────────────────────────────────────────────────────
 // STEP 1: After deploying the contract, paste your contract address here.
 // ─────────────────────────────────────────────────────────────────────────────
-const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 // ─────────────────────────────────────────────────────────────────────────────
 // ABI — copied from artifacts/contracts/Crowdfunding.sol/Crowdfunding.json
